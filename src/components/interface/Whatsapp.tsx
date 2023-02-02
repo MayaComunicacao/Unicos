837adb8eec8234ac638ebfe10b48db65a05c816d
MayaComunicacao <suporte@mayacomunicacao.com.br>
2023-02-02 11:47:14 -0300

    correcoes acessibilidade - 1

--- src/components/interface/Whatsapp.tsx
+++ src/components/interface/Whatsapp.tsx
@@ -2,7 +2,7 @@ import { SiWhatsapp } from 'react-icons/si';
 
 const WhatsApp = () => {
   return (
-    <a href="https://whatsapp.com" className="fixed bottom-6 right-6">
+    <a href="https://whatsapp.com" className="fixed bottom-6 right-6 z-50">
       <SiWhatsapp size={25} className="text-green-600" />
     </a>
   );
